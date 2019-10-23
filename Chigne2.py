15448159124b1c3e47e0a74ec5ff0308de750245
ArroyoBernilla <56905566+ArroyoBernilla@users.noreply.github.com>
2019-10-23 17:22:16 -0500

    Add files via upload

---
+++ Chigne2.py
@@ -0,0 +1,424 @@
+primer_apellido="chigne"
+print("mi primer apellido es:"+primer_apellido)
+segundo_apellido="garrampie"
+print("mi segundo apellido es:"+segundo_apellido)
+pre_nombre="Jhon kevin"
+print("mis pre nombres son:"+pre_nombre)
+sexo="masculino"
+print("mi sexo es:"+sexo)
+fecha_de_nacimiento="15/11/98"
+print("mi fecha de nacimiento es:"+fecha_de_nacimiento)
+estado_civil="soltero"
+print("mi estado civil es:"+estado_civil)
+ubigeo="130101"
+print("mi numero de ubigeo es:"+ubigeo)
+numero_de_dni="73242601"
+print("mi numero de dni:"+numero_de_dni)
+nacionalidad="republica del peru"
+print("mi nacionalidad es:"+nacionalidad)
+fecha_de_inscripcion="25/03/09"
+print("me inscribi:"+fecha_de_inscripcion)
+fecha_de_emision="06/09/16"
+print("fue emitida:"+fecha_de_emision)
+fecha_de_caducidad="06/09/24"
+print("fuecha de caducidad:"+fecha_de_caducidad)
+departamento="lambayeque"
+print("soy del departamento de:"+departamento)
+provincia="chiclayo"
+print("soy de la provincia de:"+provincia)
+distrito="la victoria"
+print("del distrito de:"+distrito)
+dirreccion="inca yupanqui"
+print("mi direcion:"+dirreccion)
+observaciones="donador de organos"
+print("como obserbacion soy:"+observaciones)
+grupo_de_votacion="281024"
+print("mi grupo de votacion es:"+grupo_de_votacion)
+numero_de_tarjeta="4557 8809 9609 7428"
+print("mi numero de tarjeta es:"+numero_de_tarjeta)
+fecha_de_vencimiento="ene-23"
+print("fecha de vencimiento de la tarjeta:"+fecha_de_vencimiento)
+numero_de_seguridad="***"
+print("numero de seguridad:"+numero_de_seguridad)
+resibo_numero="251-3887699"
+print("numerp de resivo de luz:"+resibo_numero)
+consultas="25-344422"
+print("numero de consultas para luz:"+consultas)
+fecha_del_resivo="sep-19"
+print("fecha de recivo luz:"+fecha_del_resivo)
+codigo_luz="6602-60690-8850"
+print("codigo del resivo de luz:"+codigo_luz)
+dirreccion_luz="san martin 250"
+print("direccion de ensa:"+dirreccion_luz)
+dirigido_a="torres"
+print("el documento va dirigido para:"+dirigido_a)
+p_jj="inca yupanqui"
+print("la direccion a la cual se envia:"+p_jj)
+nombre_dela_empresa="ENSA"
+print("nombre de la empresa:"+nombre_dela_empresa)
+numero_de_ruc="2.01031E"
+print("numero ruc de ensa:"+numero_de_ruc)
+empresa_de="servicion publicos"
+print("la empresa es de:"+empresa_de)
+tension_en_volteos="220v"
+print("la tension en volteos es de:"+tension_en_volteos)
+sed="d-201353"
+print("id del documento:"+sed)
+sistema_electronico="s-201-chiclayoST"
+print("usa el sistema electronico:"+sistema_electronico)
+tipo_de_conesion="monofasica aeria"
+print("tipo de coneccion:"+tipo_de_conesion)
+tarifa="BT5B-residencial"
+print("estando en la tarifa:"+tarifa)
+medidor="95069914"
+print("numero de medidor:"+medidor)
+hilos="2"
+print("numero de hitos usados:"+hilos)
+lectura_anterior="19.405.00"
+print("lectura anterior:"+lectura_anterior)
+lectura_actual="19.595.00"
+print("lectura actual:"+lectura_actual)
+diferencia_de_lectura="190"
+print("diferencia de lectura:"+diferencia_de_lectura)
+factor="100001"
+print("con un factor de:"+factor)
+uso_de="190.kwh"
+print("uso de kwh:"+uso_de)
+uso_promedio="1954.5kwh"
+print("uso promerdio:"+uso_promedio)
+potencia_contratada="0.80kw"
+print("potencia contratada:"+potencia_contratada)
+incio_contrato="01/05/2001"
+print("inicio el contrato el:"+incio_contrato)
+fin_contrato="30/04/2020"
+print("el contrato finaliza:"+fin_contrato)
+fecha_luz="25/09/2019"
+print("fecha del resivo luz:"+fecha_luz)
+fecha_corte="17/10/2019"
+print("fecha de corte:"+fecha_corte)
+recibo_del_consumo="26/09-27/10"
+print("numero del resivo:"+recibo_del_consumo)
+cargo_fijo="3.23"
+print("con cargos de:"+cargo_fijo)
+cargo_por_reposicion="1.11"
+print("con cargo de reposicion de:"+cargo_por_reposicion)
+activa="110.56"
+print("con un monto activo de:"+activa)
+alumbrado_publico="9.6"
+print("un monto para el alumbrado publico de:"+alumbrado_publico)
+alicuota="0.384"
+print("la alicuiota de:"+alicuota)
+intereses="0.22"
+print("con intereses de:"+intereses)
+sub_total="124.72"
+print("sub total de:"+sub_total)
+impuesto_a_la_venta="22.45"
+print("impuestos a la venta de:"+impuesto_a_la_venta)
+deflacion="-0.03"
+print("con una delfacion de:"+deflacion)
+regular_mensual="-0.74"
+print("un monto regular mensual de:"+regular_mensual)
+ley_luz="1.6"
+print("pago por ley para cuota de luz:"+ley_luz)
+numero_de_ley="27510"
+print("numero de ley luz:"+numero_de_ley)
+cina_principal="san jose 48"
+print("oficina principal:"+cina_principal)
+cina_pimentel="balta 501"
+print("oficina de pimentel:"+cina_pimentel)
+monsefu="san martin 128"
+print("oficina de monsefu:"+monsefu)
+reque="sucre 419"
+print("oficina de reque:"+reque)
+jose="calle miguel grau 424"
+print("oficina de san jose:"+jose)
+eten="bolognesi 609"
+print("oficina de eten:"+eten)
+horario_de_atencion="lunes a viernes"
+print("dias de atencion:"+horario_de_atencion)
+hrs_de_atns="9.00-17.00"
+print("horas de atencion:"+hrs_de_atns)
+fin_semana="sabados"
+print("tambien atendemos:"+fin_semana)
+horas_sabados="8.00-12.30"
+print("horas de atencion sabado:"+horas_sabados)
+vigente="23/05/2025"
+print("la empresa estara vigente hasta"+vigente)
+n_de_resolucion="269-2014-os/CD"
+print("numero de resolucion:"+n_de_resolucion)
+agente="banco de la nacion"
+print("agente donde se paga:"+agente)
+numero_baucher="744216568"
+print("numero de baucher:"+numero_baucher)
+lote="0.20"
+print("lote del usuario:"+lote)
+termino_del_contrato="01/05/2020"
+print("termino del contrato:"+termino_del_contrato)
+ref="481672"
+print("refemcia:"+ref)
+app="469290"
+print("el codigo app es:"+app)
+hora="15.55"
+print("hora en la que resivio el resivo:"+hora)
+cod_clinete="932507982"
+print("el codigo de cliente es:"+cod_clinete)
+numero_de_factura="t001-0687387422"
+print("el numero de factura es:"+numero_de_factura)
+n_porciento="0.01"
+print("numero de porciento:"+n_porciento)
+vence="06/05/2019"
+print("fecha de vencimento de resivo luz:"+vence)
+cliente="torres"
+print("apellido del cliente:"+cliente)
+montoadecuado="48.89"
+print("monto adecuado:"+montoadecuado)
+pensatorio="0"
+print("compensatorio de:"+pensatorio)
+tlt_pagar="48.90"
+print("total a pagar:"+tlt_pagar)
+mondo_de_pago="agente BCP"
+print("modo de pago:"+mondo_de_pago)
+calle="amaru incayupanqui"
+print("calle del cliente:"+calle)
+baucher="744246568"
+print("numero de baucher:"+baucher)
+fer="480365"
+print("numero fer:"+fer)
+ap="463927"
+print("numero de app:"+ap)
+hrs="16.48"
+print("hora de pago:"+hrs)
+print("hora de pago:"+hrs)
+fecha_claro="24/04/2019"
+print("fecha de pago claro:"+fecha_claro)
+codigo_claro="74626959"
+print("codigo de clalo:"+codigo_claro)
+factura="t001-0684687176"
+print("numero de factura claro:"+factura)
+monto="0.001"
+print("monto adicional:"+monto)
+caduca="30/04/2019"
+print("fecha de caducidad:"+caduca)
+nombre_del_cliente="chigne jhon"
+print("nombre del cliente:"+nombre_del_cliente)
+total_a_pagar="140.01"
+print("total a pagar en claro:"+total_a_pagar)
+otos="0.01"
+print("otros costos adicionales:"+otos)
+total="140.02"
+print("total a pagar en claro:"+total)
+ministro="1176478"
+print("ley ministerial del agua:"+ministro)
+resibo_agua="1-0033219777"
+print("resibo de agua:"+resibo_agua)
+reseptor="violeta"
+print("emitido para:"+reseptor)
+cod_catas="01.10.355.0350.00"
+print("codigo de catas:"+cod_catas)
+camino="100110046"
+print("codigo del camino:"+camino)
+secuencia="4 389"
+print("secuencia seguida:"+secuencia)
+rc_ensa="20103448501"
+print("ruc de ensa:"+rc_ensa)
+tlf="0800-27392"
+print("telefono de ensa:"+tlf)
+repeticion="diaria"
+print("repeticiones de envio:"+repeticion)
+servicios="agua y desague"
+print("servicios prestados de:"+servicios)
+pago_de_agua="7.53"
+print("monto de pago por agua:"+pago_de_agua)
+pago_de_desague="3.34"
+print("monto de pago desague:"+pago_de_desague)
+fijo="2 34"
+print("monto fijo:"+fijo)
+medidor_agua="8748465"
+print("numero de medidor de agua:"+medidor_agua)
+lectura_agua="826"
+print("lecura del medidor de agua:"+lectura_agua)
+lectura_12="818"
+print("lectura anterior:"+lectura_12)
+fecha_agua="19/03/2019"
+print("fecha de boleta agua:"+fecha_agua)
+caduca_fecha="17/04/2019"
+print("fecha de caducidad agua:"+caduca_fecha)
+diferencia_lectura="8"
+print("diferencia de lectura:"+diferencia_lectura)
+facturado="8"
+print("facturado:"+facturado)
+facturacion="consumo medido"
+print("facturacion por:"+facturacion)
+basto="13.19"
+print("monto basto:"+basto)
+igv="2.37"
+print("monto igv:"+igv)
+rdndo="0.04"
+print("monto de redondeo:"+rdndo)
+monto_resivido="15.6"
+print("monto resivido:"+monto_resivido)
+total_pagar="16"
+print("total a pagar por el agua:"+total_pagar)
+corte="26/04/2019"
+print("fecha de corte:"+corte)
+dia="01/04/2019"
+print("dia del resivo:"+dia)
+suministro="1176478"
+print("numero de suministro:"+suministro)
+ubicacon="saenz-peña"
+print("ubicacion de oficina principal:"+ubicacon)
+pagina_weeb="ensa.com.pe"
+print("pagina weeb:"+pagina_weeb)
+resolicion="019-2018-sunass-cd"
+print("numero de resolucion:"+resolicion)
+codigo_alumno="193680j"
+print("codigo del alumno:"+codigo_alumno)
+ciclo_academico="2019 II"
+print("ciclo academico:"+ciclo_academico)
+fecha="13/09/2019"
+print("fecha de emision:"+fecha)
+escuela_profesional="ing electronica"
+print("escuela profesional a la que ingreso:"+escuela_profesional)
+programacionI="cl10049a"
+print("codigo de programacionI:"+programacionI)
+introduccion="cl10149a"
+print("codigo de introducion a la u:"+introduccion)
+tecnicas="hu10049a"
+print("codigo de tencicas de estudio:"+tecnicas)
+numeros="mm10049a"
+print("codigo de analisis matematico:"+numeros)
+mbasica="mm10149a"
+print("codido de matematica basica:"+mbasica)
+protones="qu10049a"
+print("codigo de quimica:"+protones)
+celular="916489860"
+print("numero de celular:"+celular)
+correo="jonchigne1999"
+print("correo del alumno:"+correo)
+cretidos="22"
+print("numero total de creditos:"+cretidos)
+facultad="FACFyM"
+print("facultad a la que ingreso:"+facultad)
+tipo="4620"
+print("tipo de codigo:"+tipo)
+print("mi falculdad:"+facultad+" tipo y numerp de resibo:"+tipo+" nombre del alumno:"+nombre_del_cliente)
+escuela="electronica"
+print("escula profesional:"+escuela)
+bolante="matricula de ingreso"
+print("el presente colante es de:"+bolante)
+todo="270"
+print("el total a pagar es de:"+todo)
+print("escula a la que postula:"+escuela+" presentar:"+bolante+" monto de la matricula:"+todo)
+fecha_matricula="13/09/2019"
+print("fecha de matricula:"+fecha_matricula)
+autorisacion="020-2019-ot"
+print("codigo de autorisacion:"+autorisacion)
+ruc_facultadad="20105685875"
+print("ruc de la facultad"+ruc_facultadad)
+tiraje="3501"
+print("numero de tiraje:"+tiraje)
+litio="80"
+print("numero de litio:"+litio)
+lugar="huamachuco1380"
+print("lugar del establecimiento:"+lugar)
+fereteria="ferreteria Torres"
+print("nombre de la ferreteria:"+fereteria)
+patron="Torres delgado"
+print("nombre del dueño:"+patron)
+entorno="los Andes"
+print("lugar del establecimiento:"+entorno)
+print("monbre de la ferreteria:"+fereteria+" dueño del local"+patron+" lugar del establecimiento:"+entorno)
+de_casa="074-410967"
+print("telefono del local:"+de_casa)
+rpm="988686945"
+print("telefono de movistar:"+rpm)
+rpc="997617522"
+print("telefono de claro:"+rpc)
+numero_de_la_boleta="000-248"
+print("numero de la boleta:"+numero_de_la_boleta)
+fecha_tienda="02/10/2019"
+print("fecha de boleta tienda:"+fecha_tienda)
+cantidad1="15"
+print("catidad comprada:"+cantidad1)
+print("numero de la boleta:"+numero_de_la_boleta+" fecha de compra:"+fecha_tienda+" cantidad comprada:"+cantidad1)
+otros="16"
+print("otros montos:"+otros)
+pagar="31"
+print("total a pagar de ferreteria:"+pagar)
+n_vaucher="27342"
+print("numero de vaucher inscripcion:"+n_vaucher)
+print("otros agragados:"+otros+" total a pagar:"+pagar+" numero del vaucher:"+n_vaucher)
+telefono_apoderado="954762648"
+print("telefono del apoderado:"+telefono_apoderado)
+profesional="ing electronica"
+print("carrera a la que postula:"+profesional)
+sede="lambayeque"
+print("sede a la que postula:"+sede)
+print("el telefono del apoderado:"+telefono_apoderado+" lugar de la sede:"+sede+" postulando a la carrera:"+profesional)
+modalidad="ordinario"
+print("modalidad de ingreso:"+modalidad)
+ano_de_egreso="2015"
+print("ano en el que egreso:"+ano_de_egreso)
+coleguio_procedencia="jose maria argedas"
+print("colegui de prosedencia:"+coleguio_procedencia)
+print("modalidad de ingreso:"+modalidad+" prosediendo del colegio:"+coleguio_procedencia+" ano de egreso:"+ano_de_egreso)
+tipo_coleguio="nacional"
+print("tipo de coleguio:"+tipo_coleguio)
+nacio="chiclayo"
+print("lugar de nacimendo:"+nacio)
+luz="15/11/1998"
+print("fecha de nacimiento:"+luz)
+examen="04/08/2019"
+print("fecha del examen:"+examen)
+ugel="chiclayo"
+print("ugel:"+ugel)
+dni="73242601"
+print("numero de dni del postulante:"+dni)
+print("pertenese a la ugel:"+ugel+" fecha del examen:"+examen+" con DNI:"+dni)
+estudios="1,2,3,4,5"
+print("concluyo los estudios de:"+estudios)
+direcion="los tambos133"
+print("direccion del coleguio de prosedencia:"+direcion)
+comportamiento="ad"
+print("comportamiento final:"+comportamiento)
+print("estudios concluidos:"+estudios+" direcion del coleguio:"+direcion+" cteniendo un comportamiento:"+comportamiento)
+n_de_resolucion="269-2014-os/CD"
+print("numero de resolucion:"+n_de_resolucion)
+anos_edad="19"
+print("tengo:"+anos_edad)
+grupo_sangineo="o"
+print("mi grupo sangineo es:"+grupo_sangineo)
+cargo_fijo="3.23"
+print("con cargos de:"+cargo_fijo)
+cargo_por_reposicion="1.11"
+print("con cargo de reposicion de:"+cargo_por_reposicion)
+nombrecompleto="chigne garrampie jhon kevin"
+print("mi nombre completo es:"+nombrecompleto)
+rfrnc="policlinico"
+print("referencia de direccion:"+rfrnc)
+numero_actual="916489860"
+print("numero actual:"+numero_actual)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
